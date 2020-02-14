274a0f9eb504ce8dc6aef73cf4f64be37ea55136
Armandt <fouriearmandt@gmail.com>
2020-02-14 10:15:47 +0200

    Graph is shown with axis titles and values at the little tick marks

---
+++ LED/Debug/Drivers/BSP/Components/ft6x06/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/Components/ft6x06/ft6x06.c 
+
+OBJS += \
+./Drivers/BSP/Components/ft6x06/ft6x06.o 
+
+C_DEPS += \
+./Drivers/BSP/Components/ft6x06/ft6x06.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/Components/ft6x06/ft6x06.o: ../Drivers/BSP/Components/ft6x06/ft6x06.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../Drivers/CMSIS/Include -I"C:/Users/fouri/STM32Cube/Repository/STM32Cube_FW_F4_V1.24.2/Drivers/BSP/Components/ili9341" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"C:/Users/fouri/STM32Cube/Repository/STM32Cube_FW_F4_V1.24.2/Drivers/BSP/STM32F429I-Discovery" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/BSP/Components/ft6x06/ft6x06.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
---
+++ LED/Debug/Drivers/BSP/Components/wm8994/subdir.mk
@@ -0,0 +1,19 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/BSP/Components/wm8994/wm8994.c 
+
+OBJS += \
+./Drivers/BSP/Components/wm8994/wm8994.o 
+
+C_DEPS += \
+./Drivers/BSP/Components/wm8994/wm8994.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/BSP/Components/wm8994/wm8994.o: ../Drivers/BSP/Components/wm8994/wm8994.c
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m4 -std=gnu11 -g3 -DUSE_HAL_DRIVER -DDEBUG -DSTM32F429xx -c -I../Drivers/CMSIS/Include -I"C:/Users/fouri/STM32Cube/Repository/STM32Cube_FW_F4_V1.24.2/Drivers/BSP/Components/ili9341" -I../Drivers/STM32F4xx_HAL_Driver/Inc -I../Core/Inc -I../Drivers/CMSIS/Device/ST/STM32F4xx/Include -I../Drivers/STM32F4xx_HAL_Driver/Inc/Legacy -I"C:/Users/fouri/STM32Cube/Repository/STM32Cube_FW_F4_V1.24.2/Drivers/BSP/STM32F429I-Discovery" -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -MMD -MP -MF"Drivers/BSP/Components/wm8994/wm8994.d" -MT"$@" --specs=nano.specs -mfpu=fpv4-sp-d16 -mfloat-abi=hard -mthumb -o "$@"
+
